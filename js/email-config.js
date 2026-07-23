9ab83df81046f90c3571c0c971cb6c2744debb6b
M.Abdullah_Anwar <mabdullahanwar85@gmail.com>
2026-07-23 10:14:09 +0500

    Fix EmailJS order notification system

--- js/email-config.js
+++ js/email-config.js
@@ -1,5 +1,8 @@
 const EMAILJS_PUBLIC_KEY = 'ZdJKUgS5zBMWMinIF';
-const EMAILJS_SERVICE_ID = 'service_vcyr5vp';
+const EMAILJS_SERVICE_ID = 'service_vcvr5vp';
 const EMAILJS_TEMPLATE_ID = 'template_hvu65e9';
 const STORE_OWNER_EMAIL = 'mabdullahanwar85@gmail.com';
 const ADMIN_URL = window.location.origin + '/admin.html';
+emailjs.init({
+    publicKey: EMAILJS_PUBLIC_KEY
+});
